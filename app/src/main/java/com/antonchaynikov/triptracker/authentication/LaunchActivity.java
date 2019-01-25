09b1178b6d954af406ae68225400d67f110affc2
Anton Chaynikov <anton.chaynikov@flatstack.com>
2019-01-25 13:51:58 +0300

    Updated mockito config

--- app/src/main/java/com/antonchaynikov/triptracker/authentication/LaunchActivity.java
+++ app/src/main/java/com/antonchaynikov/triptracker/authentication/LaunchActivity.java
@@ -3,12 +3,15 @@ package com.antonchaynikov.triptracker.authentication;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import io.fabric.sdk.android.Fabric;
+import com.crashlytics.android.Crashlytics;
 import com.antonchaynikov.triptracker.R;
 
 public class LaunchActivity extends AppCompatActivity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.frame_layout);
         getSupportFragmentManager()
                 .beginTransaction()
