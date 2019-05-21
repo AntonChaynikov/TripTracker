8f362b6b8a0aa87239457a1b08b8a9db1190c345
AntonChaynikov <anton.chaynikov@flatstack.com>
2019-05-21 19:55:29 +0300

    Updated Dagger config

--- app/src/main/java/com/antonchaynikov/triptracker/NewHistoryComponent.kt
+++ app/src/main/java/com/antonchaynikov/triptracker/NewHistoryComponent.kt
@@ -3,10 +3,18 @@ package com.antonchaynikov.triptracker
 import com.antonchaynikov.tripshistory.HistoryFragment
 import com.antonchaynikov.tripshistory.TripHistoryModule
 import com.antonchaynikov.triptracker.application.NewAppComponent
+import dagger.BindsInstance
 import dagger.Component
 
 @Component(dependencies = [NewAppComponent::class], modules = [TripHistoryModule::class])
 interface NewHistoryComponent {
+    @Component.Builder
+    interface Builder {
+        fun appComponent(appComponent: NewAppComponent): Builder
+        @BindsInstance
+        fun historyFragment(historyFragment: HistoryFragment): Builder
+        fun build(): NewHistoryComponent
+    }
 
     fun inject(historyFragment: HistoryFragment)
 }
