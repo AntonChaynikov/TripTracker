673d4130d6408e59a6a4386e6b2b4f1f7c306303
Anton Chaynikov <anton.chaynikov@flatstack.com>
2019-06-24 17:38:47 +0300

    Added about us fragment

---
+++ about/src/main/java/com/antonchainikov/about/AboutUsFragment.kt
@@ -0,0 +1,15 @@
+package com.antonchainikov.about
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class AboutUsFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        //return inflater.inflate(R.layout.fragment_about_us, container, false)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
